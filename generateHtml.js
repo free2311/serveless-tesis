fbb6c476232f4f11c810fd96d0af19ff7cdf321f
kevin-tooles <kevinvillalba@tooles.co>
2023-04-24 21:30:29 -0500

    test commit

---
+++ generateHtml.js
@@ -0,0 +1,13 @@
+
+function generateEmailHtml() {
+  return `
+      <html>
+        <body>
+          <h1>Bienvenido a mi correo electrónico</h1>
+          <a href="https://google.com">Website</a>
+        </body>
+      </html>
+    `;
+}
+
+module.exports = generateEmailHtml;
--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 require('dotenv').config();
+const nodemailer = require('nodemailer');
+const connection = require("./db");
+const generateEmailHtml = require('./generateHtml');
+
+/**configurar gmail */
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'kevinazul999@gmail.com',
+    pass: 'efpcvhxhsrxsrqns'
+  }
+});
 
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Welcome ${process.env.PORT? process.env.PORT : "test" }`,
-        input: event,
-      },
-      null,
-      2
-    ),
+
+  connection.query('select * from admin_clients', async (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(401).json({ status: false, message: "error", data: [result] })
+
+    } else {
+      console.log(result);
+    }
+
+  })
+
+  let mailOptions = {
+    from: 'kevinazul999@gmail.com',
+    to: 'kevinazul9999@gmail.com',
+    subject: 'Correo de prueba con HTML',
+    html: generateEmailHtml()
   };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return {
+        message: error
+      };
+    } else {
+      console.log('Correo electrónico enviado: ' + info.response);
+      return {
+        message: "prueba exitosa"
+      };
+    }
+  });
+
+
 };
+
