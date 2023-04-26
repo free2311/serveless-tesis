1309b05b3a0f989d60ce14e0b7f3fb88020f81b5
kevindev1110 <kevinazul9999@gmail.com>
2023-04-26 16:24:52 -0500

    ajustes envio en lamda

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
 require('dotenv').config();
-const nodemailer = require('nodemailer');
-const connection = require("./db");
+var nodemailer = require('nodemailer');
+//const connection = require("./db");
 const generateEmailHtml = require('./generateHtml');
 
-/**configurar gmail */
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'kevinazul999@gmail.com',
-    pass: 'efpcvhxhsrxsrqns'
+module.exports.handler = async () => {
+
+  try {
+    console.log("hello");
+    let transporter = nodemailer.createTransport({
+      service: 'outlook',
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "kyvillalban@itc.edu.co",
+        pass: "2190244.Kv"
+      }
+    });
+
+    var mailOptions = {
+      from: 'kyvillalban@itc.edu.co',
+      to: '<kevinazul9999@gmail.com>',
+      bcc: '<bcc email addres>',
+      subject: 'Test subject',
+      html: `<p>hello</p>`
+    };
+
+    const response = await new Promise((rsv, rjt) => {
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return rjt(error)
+        }
+        console.log(info);
+      });
+    });
+
+  } catch (error) {
+    console.log(error);
   }
-});
-
-module.exports.handler = async (event) => {
-
-  connection.query('select * from admin_clients', async (error, result) => {
-    if (error) {
-      console.log(error);
-      res.status(401).json({ status: false, message: "error", data: [result] })
-
-    } else {
-      console.log(result);
-    }
-
-  })
-
-  let mailOptions = {
-    from: 'kevinazul999@gmail.com',
-    to: 'kevinazul9999@gmail.com',
-    subject: 'Correo de prueba con HTML',
-    html: generateEmailHtml()
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-      return {
-        message: error
-      };
-    } else {
-      console.log('Correo electrónico enviado: ' + info.response);
-      return {
-        message: "prueba exitosa"
-      };
-    }
-  });
 
 
-};
+
+}
 
